2c979a9cd88fabbc209f289c2629881d33c92105
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-10-25 16:51:28 +0600

    create backend server

--- Back End/app.js
+++ Back End/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = require('cors'); // Importing cors
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const DBConnection = require('./config/DBconnection');
 const { notFound, defaultErrorHandler } = require('./Middlewear/ErrorHandler/errorHandler');
@@ -7,7 +7,7 @@ const app = express();
 const port = 3000;
 
 app.use(cors({
-    origin: "http://localhost:5173/",
+    origin: "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization", "Origin", "Accept"],
