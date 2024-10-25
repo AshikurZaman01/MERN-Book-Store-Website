2ebab13bd64680b0267847137297ad2157d0d8b3
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-10-25 10:23:51 +0600

    add Register mage

--- Front End/src/Components/Login/RegisterWithGoogle.jsx
+++ Front End/src/Components/Login/RegisterWithGoogle.jsx
@@ -5,7 +5,7 @@ const RegisterWithGoogle = () => {
         <div>
             <button className="w-full flex justify-center items-center bg-secondary hover:bg-blue-700 text-white font-bold py-2 rounded focus:outline-none transition duration-200">
                 <FaGoogle className="mr-2" />
-                <span>Register With Google</span>
+                <span>SignIn With Google</span>
             </button>
         </div>
     )
