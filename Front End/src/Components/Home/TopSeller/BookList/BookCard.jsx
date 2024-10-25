fb54c0d5f6333c040546c98efc8a628f36509e6c
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-10-25 11:41:33 +0600

    make BookCard btn is Added to cart

--- Front End/src/Components/Home/TopSeller/BookList/BookCard.jsx
+++ Front End/src/Components/Home/TopSeller/BookList/BookCard.jsx
@@ -1,26 +1,25 @@
 import { FiShoppingCart } from 'react-icons/fi';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import getImageURL from '../../../../Utils/getImageURL';
 import { useDispatch } from 'react-redux';
 import { addToCart } from '../../../../Redux/features/Cart/cartSlice';
+import { useState } from 'react';
 
 const BookCard = ({ book, isLoading }) => {
-
     const dispatch = useDispatch();
+    const [isAdded, setIsAdded] = useState(false);
 
     const handleAddToCart = (book) => {
         dispatch(addToCart(book));
+        setIsAdded(true); 
     }
 
     return (
-        <div className=" rounded-lg transition-shadow duration-300">
-            <div
-                className="flex flex-col sm:flex-row sm:items-center sm:h-72  sm:justify-center gap-4"
-            >
+        <div className="rounded-lg transition-shadow duration-300">
+            <div className="flex flex-col sm:flex-row sm:items-center sm:h-72 sm:justify-center gap-4">
                 <div className="sm:h-72 sm:flex-shrink-0 border rounded-md">
                     <Link to={`/books/${book?._id}`}>
                         <img
-
                             src={`${getImageURL(book.coverImage)}`}
                             alt=""
                             className="w-[200px] h-full bg-cover p-2 rounded-md cursor-pointer hover:scale-105 transition-all duration-200"
@@ -34,20 +33,24 @@ const BookCard = ({ book, isLoading }) => {
                             {book?.title}
                         </h3>
                     </Link>
-                    <p className="text-gray-600 mb-5">{book?.description.length > 80 ? `${book.description.slice(0, 80)}...` : book?.description}</p>
+                    <p className="text-gray-600 mb-5">
+                        {book?.description.length > 80 ? `${book.description.slice(0, 80)}...` : book?.description}
+                    </p>
                     <p className="font-medium mb-5">
                         ${book?.newPrice} <span className="line-through font-normal ml-2">$ {book?.oldPrice}</span>
                     </p>
                     <button
                         onClick={() => handleAddToCart(book)}
-                        className="bg-primary px-6 py-2 rounded-md btn btn-md space-x-1 flex items-center gap-1 ">
-                        <FiShoppingCart className="" />
-                        <span>Add to Cart</span>
+                        className={`bg-primary px-6 py-2 rounded-md btn btn-md space-x-1 flex items-center gap-1 ${isAdded ? 'disabled' : ''}`}
+                        disabled={isAdded} 
+                    >
+                        <FiShoppingCart />
+                        <span>{isAdded ? 'Added to Cart' : 'Add to Cart'}</span>
                     </button>
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
-export default BookCard
+export default BookCard;
